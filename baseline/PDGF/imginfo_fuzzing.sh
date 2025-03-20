1ec91bfbe502241f552c9d984e31c406ba06d82b
null1024-ws <shenwang918@gmail.com>
2025-03-20 05:47:22 +0000

    add imginfo

---
+++ baseline/PDGF/imginfo_fuzzing.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+set -e
+
+TARGET_FILE=$1
+if [ -z "$TARGET_FILE" ]; then
+    echo "Usage: $0 <target>"
+    exit 1
+fi
+
+rm -rf jasper-2.0.12
+tar -xvf jasper-2.0.12.tar.gz
+
+# compile
+cd jasper-2.0.12
+CC=~/gllvm/gclang CXX=~/gllvm/gclang++ 
+cmake -DJAS_ENABLE_SHARED=OFF -DALLOW_IN_SOURCE_BUILD=ON .
+make -j
+~/gllvm/get-bc ./src/appl/imginfo
+mkdir fuzz
+cd fuzz
+cp ../src/appl/imginfo.bc .
+echo "$TARGET_FILE" > targets
+~/PDGF/instrument/bin/cbi -targets=targets imginfo.bc
+~/PDGF/fuzz/afl-clang-fast imginfo.bc -o imginfo.ci -lm
+
+# prepare the input
+mkdir in
+cp -r ../../seeds/general_evaluation/imginfo/* in/
+echo 1 > pre_edges.txt
+pre_edges=$(cat pre_edges.txt)
+
+~/PDGF/fuzz/afl-fuzz -i in -o out -e "$pre_edges" -- ./imginfo.ci -f @@
