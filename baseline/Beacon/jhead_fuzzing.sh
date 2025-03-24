71163c7ea6f62688612bd89c345a5ef333befccf
null1024-ws <shenwang918@gmail.com>
2025-03-24 07:55:38 +0000

    add jhead-3.00

---
+++ baseline/Beacon/jhead_fuzzing.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+set -e
+
+TARGET_FILE=$1
+if [ -z "$TARGET_FILE" ]; then
+    echo "Usage: $0 <target>"
+    exit 1
+fi
+
+rm -rf jhead-3.00
+tar -xvf jhead-3.00.tar.gz
+
+# get the bc file
+cd jhead-3.00
+
+export CC=wllvm
+export CXX=wllvm++
+export LLVM_COMPILER=clang
+
+make clean
+make CFLAGS="-g" -j # mandatory flags
+extract-bc jhead
+
+mkdir -p jhead_fuzzing
+cd jhead_fuzzing
+
+# prepare the target line
+echo "$TARGET_FILE" > cstest.txt
+BASENAME=$(echo "$TARGET_FILE" | awk -F: '{print $1}' | sed 's/\.c//')
+LINENUM=$(echo "$TARGET_FILE" | awk -F: '{print $2}')
+
+# start the static analysis
+/Beacon/precondInfer ../jhead.bc --target-file=cstest.txt --join-bound=5 >precond_log 2>&1
+# instrument
+/Beacon/Ins -output=jhead_${BASENAME}_${LINENUM}.bc -blocks=bbreaches.txt -afl -log=log.txt -load=range_res.txt transed.bc
+
+# compile
+clang jhead_${BASENAME}_${LINENUM}.bc -o jhead_${BASENAME}_${LINENUM} -lm -lz /Beacon/Test/afl-llvm-rt.o
+
+# prepare the seeds
+mkdir in
+cp -r ../../seeds/general_evaluation/jhead/* in/ 
+
+# start fuzzing
+export AFL_SKIP_CPUFREQ=1 # you can comment this line
+/Beacon/afl-fuzz -i in -o out -m none -t 99999 -d -- ./jhead_${BASENAME}_${LINENUM} @@
