78756d6b932ee40de97cb60e5bd9f007bc64e0a2
null024 <79917102+null1024-ws@users.noreply.github.com>
2025-03-25 22:17:04 +0800

    Update set_magma.sh

--- baseline/PDGF/set_magma.sh
+++ baseline/PDGF/set_magma.sh
@@ -52,7 +52,7 @@ git clone https://github.com/null1024-ws/libtiff-magma.git libtiff
 git clone https://github.com/null1024-ws/libxml2-magma.git libxml2
 cd libxml2
 # comment unrelated log function
-for file in parser.c valid.c; do
+for file in parser.c valid.c xmlreader.c xinclude.c; do
     sed -i 's/.*mdafl_gc_log.*/\/\/ &/' "$file"
 done
 # git -C libxml2 checkout ec6e3efb06d7b15cf5a2328fabd3845acea4c815
