5c2ef92ff6bebb602f60f4470f0b582741a00dc8
null1024-ws <shenwang918@gmail.com>
2025-03-31 05:47:22 +0000

    revise libpng

--- baseline/PDGF/libpng_fuzzing.sh
+++ baseline/PDGF/libpng_fuzzing.sh
@@ -7,24 +7,21 @@ if [ -z "$TARGET_FILE" ]; then
     exit 1
 fi
 
-# Instead of using -static-libasan, we manually force static linking for libasan.
-export LDFLAGS="-fsanitize=address -Wl,-Bstatic -lasan -Wl,-Bdynamic"
-export CFLAGS="-g -fno-omit-frame-pointer -fcommon -Wno-error -fsanitize=address -Wno-strict-prototypes"
-export CXXFLAGS="-g -fno-omit-frame-pointer -fcommon -Wno-error -fsanitize=address -Wno-strict-prototypes"
+
+# export LDFLAGS="-fsanitize=address -Wl,-Bstatic -lasan -Wl,-Bdynamic"
+export CFLAGS="-g -fno-omit-frame-pointer -fcommon -Wno-error -Wno-strict-prototypes"
+export CXXFLAGS="-g -fno-omit-frame-pointer -fcommon -Wno-error  -Wno-strict-prototypes"
 
 cd libpng
-CC=~/gllvm/gclang
-CXX=~/gllvm/gclang++
+export CC=~/gllvm/gclang
+export CXX=~/gllvm/gclang++
 
 autoconf -f -i
 ./configure --disable-shared
 
 make clean
-# Ensure both compile and link phases include our ASAN flags.
-make CC=~/gllvm/gclang CXX=~/gllvm/gclang++ \
-     CFLAGS="-fsanitize=address" \
-     LDFLAGS="-fsanitize=address -Wl,-Bstatic -lasan -Wl,-Bdynamic" \
-     -j1 libpng-read-fuzzer
+
+make -j1 libpng-read-fuzzer
 
 ~/gllvm/get-bc ./libpng-read-fuzzer
 
@@ -35,9 +32,14 @@ cp ../libpng-read-fuzzer.bc .
 echo "$TARGET_FILE" > targets
 
 ~/PDGF/instrument/bin/cbi -targets=targets libpng-read-fuzzer.bc
-~/PDGF/fuzz/afl-clang-fast libpng-read-fuzzer.bc -o libpng-read-fuzzer.ci
+~/PDGF/fuzz/afl-clang-fast libpng-read-fuzzer.bc -o libpng-read-fuzzer.ci -lm -lz
 
 mkdir in
 cp -r ../../seed/libpng/corpus/libpng_read_fuzzer/* in/
+
+if [ ! -f pre_edges.txt ] || [ "$(cat pre_edges.txt)" -eq 0 ]; then
+     echo 1 > pre_edges.txt  # make sure the fuzzing can start
+fi
+
 pre_edges=$(cat pre_edges.txt)
 ~/PDGF/fuzz/afl-fuzz -i in -o out -e "$pre_edges" -- ./libpng-read-fuzzer.ci @@
